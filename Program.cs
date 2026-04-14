ce8081c9d07b86c6b99225bbb598830509787b83
zeyad tarek <vetogamer5@outlook.com>
2026-04-14 13:04:49 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace ConsoleApp1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //    List<Product> ProductList = new List<Product>
+            //{
+            //    new Product { ProductName = "Chai", Category = "Beverages", UnitPrice = 18, UnitsInStock = 39 },
+            //    new Product { ProductName = "Chang", Category = "Beverages", UnitPrice = 19, UnitsInStock = 17 },
+            //    new Product { ProductName = "Aniseed Syrup", Category = "Condiments", UnitPrice = 10, UnitsInStock = 13 },
+            //    new Product { ProductName = "Chef Anton's Cajun Seasoning", Category = "Condiments", UnitPrice = 22, UnitsInStock = 53 },
+            //    new Product { ProductName = "Ikura", Category = "Seafood", UnitPrice = 31, UnitsInStock = 20 },
+            //    new Product { ProductName = "Konbu", Category = "Seafood", UnitPrice = 6, UnitsInStock = 24 }
+            //};
+
+            //    var seafoodProducts = ProductList
+            //    .Where(p => p.Category == "Seafood")
+            //    .Select(p => new { p.ProductName, p.UnitPrice });
+
+            //    foreach (var p in seafoodProducts)
+            //    {
+            //        Console.WriteLine($"{p.ProductName} - {p.UnitPrice}");
+            //    } 
+            #endregion
         }
     }
 }
