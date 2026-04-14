d81d1ee2d3d722e4adeec33b9346c6907c4ccf9e
zeyad tarek <vetogamer5@outlook.com>
2026-04-14 13:07:35 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<Product> ProductList = new List<Product>
+            {
+                new Product { ProductName = "Chai", Category = "Beverages", UnitPrice = 18, UnitsInStock = 39 },
+                new Product { ProductName = "Chang", Category = "Beverages", UnitPrice = 19, UnitsInStock = 17 },
+                new Product { ProductName = "Aniseed Syrup", Category = "Condiments", UnitPrice = 10, UnitsInStock = 13 },
+                new Product { ProductName = "Chef Anton's Cajun Seasoning", Category = "Condiments", UnitPrice = 22, UnitsInStock = 53 },
+                new Product { ProductName = "Ikura", Category = "Seafood", UnitPrice = 31, UnitsInStock = 20 },
+                new Product { ProductName = "Konbu", Category = "Seafood", UnitPrice = 6, UnitsInStock = 24 }
+            };
             #region Q1
 
             //    var seafoodProducts = ProductList
@@ -18,6 +27,17 @@ namespace ConsoleApp1
             //        Console.WriteLine($"{p.ProductName} - {p.UnitPrice}");
             //    } 
             #endregion
+
+            #region Q2
+            //var productNames = ProductList.Select(p => p.ProductName);
+
+            //foreach (var name in productNames)
+            //{
+            //    Console.WriteLine(name); 
+            #endregion
+
+
+        }
         }
     }
-}
+
